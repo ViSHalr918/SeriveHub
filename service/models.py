748840c1ce1c8db27ba11f985fe871176ad05315
vishal <vishalr9395@gmail.com>
2024-10-01 13:54:48 +0530

    models updated again

--- service/models.py
+++ service/models.py
@@ -1,56 +1,45 @@
 from django.db import models
-
 from django.contrib.auth.models import User
 
 # Create your models here.
-work_choice = (
-    "pending","pending",
-    "in_progress","in_progress",
-    "completed","completed"
-)
 
-class CustomerModel(models.Model):
-
-    name = models.CharField(max_length=100)
-
-    phone = models.CharField(max_length=15)
-
+class Customer(models.Model):
+    
+    name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=100)
     email = models.EmailField()
-
-    vechicle_number = models.CharField(max_length=30)
-
-    running_kilometer = models.IntegerField()
-
-    work_status = models.CharField(choices=work_choice,default="pending",max_length=100)
-
-    created_date = models.DateField(auto_now_add=True)
-
-    update_date = models.DateTimeField(auto_now=True)
-
-    is_active = models.BooleanField(default=True)
-
+    vehicle_number = models.CharField(max_length=200)
+    running_kilometer = models.PositiveIntegerField()
+    work_status_choices =(
+        ("pending","pending"),
+        ("in_progress","in_progress"),
+        ("completed","completed")
+    )
+        
+    
+    work_status = models.CharField(max_length=200, choices=work_status_choices,default="pending")
+    
     service_advisor = models.ForeignKey(User,on_delete=models.CASCADE)
-
+    
+    created_date = models.DateTimeField(auto_now_add=True)
+    
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    is_active = models.BooleanField(default=True)
+    
     def __str__(self):
         return self.name
-
-
-
-class WorkModel(models.Model):
-
-    customer_object = models.ForeignKey(CustomerModel,on_delete=models.CASCADE)
-
-    description = models.CharField(max_length=400)
-
+    
+class Work(models.Model):
+    
+    customer_object = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    
+    description = models.CharField(max_length=300)
+    
     amount = models.FloatField()
-
-    date_created = models.DateField(auto_now=True)
-
-    is_active = models.BooleanField(default=False)
-
-    created_date = models.DateField(auto_now_add=True)
-
-    update_date = models.DateTimeField(auto_now=True)
-
+    
+    created_date = models.DateTimeField(auto_now_add=True)
+    
+    updated_date = models.DateTimeField(auto_now=True)
+    
     is_active = models.BooleanField(default=True)
-
